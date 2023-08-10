d7d955ad60b7a828d26ae9881e1011c2630efb9f
Andrianata Daud <andrianta.321@gmail.com>
2023-08-10 12:43:52 +0700

    add CRUD cart with fix relation

--- features/user/handler/handler.go
+++ features/user/handler/handler.go
@@ -4,9 +4,7 @@ import (
 	"ecommerce/features/user"
 	"ecommerce/helper"
 	"fmt"
-	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -33,7 +31,7 @@ func (uc *userControll) Register() echo.HandlerFunc {
 			return c.JSON(PrintErrorResponse(err.Error()))
 		}
 
-		return c.JSON(http.StatusCreated, "Registered a new account successfully")
+		return c.JSON(PrintSuccessNoData(http.StatusCreated, "Registered a new account successfully"))
 	}
 }
 
@@ -55,35 +53,16 @@ func (uc *userControll) Login() echo.HandlerFunc {
 	}
 }
 
-func (uc *userControll) GetData() echo.HandlerFunc {
+func (uc *userControll) Profile() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		token := c.Get("user")
 
-		res, err := uc.srv.GetData(token)
-
-		if err != nil {
-			return c.JSON(PrintErrorResponse(err.Error()))
-		}
-
-		return c.JSON(PrintSuccessReponse(http.StatusOK, "Displayed your profile successfully", res))
-	}
-}
-
-func (uc *userControll) Profile() echo.HandlerFunc {
-	return func(c echo.Context) error {
-		id, err := strconv.Atoi(c.Param("id"))
-
-		if err != nil {
-			log.Println("convert id error", err.Error())
-			return c.JSON(http.StatusBadGateway, "Invalid input")
-		}
-
-		res, err := uc.srv.Profile(uint(id))
+		res, err := uc.srv.Profile(token)
 
 		if err != nil {
 			return c.JSON(helper.PrintErrorResponse(err.Error()))
 		}
-		return c.JSON(helper.PrintSuccessReponse(http.StatusCreated, "success get user content", res))
+		return c.JSON(helper.PrintSuccessReponse(http.StatusCreated, "Displayed your profile successfully", res))
 	}
 }
 
@@ -123,6 +102,6 @@ func (uc *userControll) Deactivate() echo.HandlerFunc {
 			return c.JSON(PrintErrorResponse(err.Error()))
 		}
 
-		return c.JSON(http.StatusAccepted, "Deactivated your account successfully")
+		return c.JSON(PrintSuccessNoData(http.StatusAccepted, "Deactivated your account successfully"))
 	}
 }
